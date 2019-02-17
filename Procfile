f524eb5fadba24b8cfe10edf6ad784c6d3658c8a
Hameed Khan <vjhameed3@gmail.com>
2019-02-17 03:08:55 -0800

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn facebookpages.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- facebookPages/settings.py
+++ facebookPages/settings.py
@@ -134,30 +134,35 @@ WSGI_APPLICATION = 'facebookPages.wsgi.application'
 #     }
 # }
 
+import dj_database_url
+
+DATABASES = { 'default' : dj_database_url.config()}
+
+
 DATABASES = {
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ.get('DB_NAME', 'fbapp'),
-    #     'USER': os.environ.get('DB_USER', 'postgres'),
-    #     'PASSWORD': os.environ.get('DB_PASSWORD', '123456789'),
-    #     'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-    #     'PORT': os.environ.get('DB_PORT', '5432'),
-    #     'TEST': {
-    #         'NAME': 'test_default',
-    #         'USER': 'postgres',
-    #         'PASSWORD': 'postgres',
-    #         'HOST': 'db',
-    #         'PORT': 5432
-    #     },
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'searchfb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.environ.get('DB_NAME', 'fbapp'),
+        # 'USER': os.environ.get('DB_USER', 'postgres'),
+        # 'PASSWORD': os.environ.get('DB_PASSWORD', '123456789'),
+        # 'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        # 'PORT': os.environ.get('DB_PORT', '5432'),
+        # 'TEST': {
+        #     'NAME': 'test_default',
+        #     'USER': 'postgres',
+        #     'PASSWORD': 'postgres',
+        #     'HOST': 'db',
+        #     'PORT': 5432
+        # },
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'searchfb',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
