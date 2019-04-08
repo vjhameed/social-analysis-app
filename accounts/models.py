f3107035534172c1b9a395c39c612fb11d6d66be
Muhammad Asif <asif@csinpakistan.com>
2019-04-08 11:26:36 +0500

    minor fix

--- accounts/models.py
+++ accounts/models.py
@@ -14,15 +14,13 @@ class UserManager(BaseUserManager):
         # return self.create_user(email, password, is_staff=False, is_superuser=False, is_active=True,
         # **extra_fields)
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self.create_user(email, password, is_staff=True, is_superuser=True, is_active=True,
+                                **extra_fields)
 
-def create_superuser(self, email, password=None, **extra_fields):
-    return self.create_user(email, password, is_staff=True, is_superuser=True, is_active=True,
-                            **extra_fields)
-
-
-def create_business_user(self, email, password=None, **extra_fields):
-    return self.create_user(email, password, is_staff=False, is_superuser=False, is_active=True,
-                            **extra_fields)
+    def create_business_user(self, email, password=None, **extra_fields):
+        return self.create_user(email, password, is_staff=False, is_superuser=False, is_active=True,
+                                **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
